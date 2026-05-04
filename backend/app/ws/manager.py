642b7b05c3f4830999b10995be9e116501aa0adf
lucarl07 <junitinho2007@gmail.com>
2026-05-04 16:19:23 -0300

    refactor: Modularize backend's routes & WS connection manager
    WS = WebSocket

---
+++ backend/app/ws/manager.py
@@ -0,0 +1,21 @@
+# Bibliotecas do Projeto
+from fastapi import WebSocket
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket, nickname: str):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+        await self.broadcast(f'  "{nickname}" entrou no chat.')
+
+    def disconnect(self, websocket: WebSocket, nickname: str):
+        self.active_connections.remove(websocket)
+
+    async def broadcast(self, content: str):
+        for connection in self.active_connections:
+            await connection.send_text(content)
+
+manager = ConnectionManager()
+
