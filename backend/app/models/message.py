f053a27b59bec42e56e12d779a1959c52b77ef53
lucarl07 <junitinho2007@gmail.com>
2026-05-04 17:38:56 -0300

    feat: New table model Message

---
+++ backend/app/models/message.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, DateTime
+from datetime import datetime, timezone
+from uuid import uuid4
+from app.db import Base
+
+class Message(Base):
+    __tablename__ = "messages"
+
+    id: Mapped[str] = mapped_column(String(36), primary_key=True, default=lambda: str(uuid4()))
+    nickname: Mapped[str] = mapped_column(String(50), nullable=False)
+    content: Mapped[str] = mapped_column(String(2000), nullable=False)
+    timestamp: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc)
+    )
