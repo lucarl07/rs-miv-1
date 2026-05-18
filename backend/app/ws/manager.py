081b3fabd78a2f4109c9c86aac82f7f487f924e5
lucarl07 <07lucarl.dev@gmail.com>
2026-05-18 16:38:27 -0300

    refactor: Format WebSocket broadcasts as JSON

--- backend/app/ws/manager.py
+++ backend/app/ws/manager.py
@@ -1,4 +1,6 @@
 # Bibliotecas do Projeto
+import json
+from datetime import datetime, timezone
 from fastapi import WebSocket
 
 class ConnectionManager:
@@ -8,7 +10,11 @@ class ConnectionManager:
     async def connect(self, websocket: WebSocket, nickname: str):
         await websocket.accept()
         self.active_connections.append(websocket)
-        await self.broadcast(f'  "{nickname}" entrou no chat.')
+        await self.broadcast(json.dumps({
+            "nickname": "%sys%",
+            "content": f"{nickname} entrou no chat",
+            "timestamp": datetime.now(timezone.utc).isoformat()
+        }))
 
     def disconnect(self, websocket: WebSocket, nickname: str):
         self.active_connections.remove(websocket)
