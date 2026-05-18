f05bfd9247259a3b9eb634be1e07aa3c19bf27c9
lucarl07 <07lucarl.dev@gmail.com>
2026-05-18 17:51:06 -0300

    cleanup: Remove unused data array from ChatWindow SFC

--- frontend/src/components/ChatWindow.vue
+++ frontend/src/components/ChatWindow.vue
@@ -3,16 +3,6 @@
   import MessageInput from "./MessageInput.vue"
   import useWebSocket from "../composables/useWebSocket.js"
 
-  const _messages = [
-    // Array demonstrativa apenas; O template usará os dados recebidos pela API.
-    {
-      id: '10aec4a9-805b-456d-9bf2-2807aec1e783',
-      nickname: 'Fulano de Tal',
-      content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-      timestamp: new Date().toISOString()
-    },
-  ]
-
   const nickname = "luiz"
   const { status, messages, send } = useWebSocket(`ws://localhost:8000/ws?nickname=${nickname}`)
 
