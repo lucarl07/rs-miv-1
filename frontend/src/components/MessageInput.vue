e868d0fa84f855356658810896a87a816b614e4f
lucarl07 <07lucarl.dev@gmail.com>
2026-05-29 19:54:45 -0300

    feat(frontend): Style MessageInput & replace its input w/a textarea

--- frontend/src/components/MessageInput.vue
+++ frontend/src/components/MessageInput.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
   import { ref } from 'vue'
 
+  import icon_btnSend from '../assets/icons/btn_send.svg'
+
   const emit = defineEmits(['send'])
 
   const message = ref('')
@@ -14,10 +16,29 @@
 </script>
 
 <template>
-  <form @submit.prevent>
-    <input type="text" v-model="message" />
-    <button @click="handleSubmit">
-      Enviar
+  <form @submit.prevent
+    class="
+      flex h-10 rounded-sm bg-pale-silver
+      focus-within:outline-crushed-berry focus-within:outline-double
+    "
+  >
+    <textarea
+      v-model="message"
+      autofocus="true"
+      spellcheck="true"
+      minlength="1"
+      maxlength="2000"
+      class="
+        w-full h-full resize-none px-4 py-2 text-crushed-berry
+        focus:outline-none
+      "
+    />
+    <button @click="handleSubmit" type="submit" class="px-4">
+      <img
+        :src="icon_btnSend"
+        alt="Botão de enviar"
+        class="w-7 h-7"
+      />
     </button>
   </form>
 </template>
