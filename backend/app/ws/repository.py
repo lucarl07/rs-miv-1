e27e369e656beadb8d2dae2d82888eee43a11aa3
lucarl07 <07lucarl.dev@gmail.com>
2026-06-22 17:53:27 -0300

    feat(backend/repository): Return message on function save_message

--- backend/app/ws/repository.py
+++ backend/app/ws/repository.py
@@ -1,7 +1,8 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.models.message import Message
 
-async def save_message(db: AsyncSession, nickname: str, content: str):
+async def save_message(db: AsyncSession, nickname: str, content: str) -> Message:
     message = Message(nickname=nickname, content=content) # Timestamp será gerada automaticamente no modelo Message
     db.add(message)
     await db.commit()
+    return message;
