a07ee4212bc345744b915691708df1a8af0b5743
lucarl07 <07lucarl.dev@gmail.com>
2026-06-30 14:29:55 -0300

    feat(backend): Add CORS middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 # Bibliotecas do Projeto
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 # Módulos do Projeto
 from .db import Base
@@ -8,6 +9,17 @@ from .router import router
 
 app = FastAPI()
 app.include_router(router)
+
+origins = [
+    "http://localhost:5173", # Front-end local
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def startup():
