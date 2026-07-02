f7c9b96f3e0ea10b727891ff8a8ce8a2494acf9d
lucarl07 <07lucarl.dev@gmail.com>
2026-07-02 13:47:27 -0300

    feat(backend): Create async connection to Redis

---
+++ backend/app/conn/redis.py
@@ -0,0 +1,9 @@
+# Bibliotecas Nativas
+import os
+
+# Bibliotecas do Projeto
+import redis.asyncio as redis
+
+REDIS_URL = os.environ.get("REDIS_URL", "redis://localhost:6379/0")
+
+r = redis.from_url(REDIS_URL, decode_responses=True)
