7abb45868243b27f818c2e7e2024579a3cebc5c4
lucarl07 <07lucarl.dev@gmail.com>
2026-07-02 13:47:27 -0300

    feat(backend): Create async connection to Redis

--- backend/app/conn/__init__.py
+++ backend/app/conn/__init__.py
@@ -5,12 +5,20 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 # Módulos do Projeto:
+from .redis import r
 from .db import Base, engine
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    # 1. Inicializa o Redis
+    app.state.redis = r
+
+    # 2. Sincroniza tabelas do banco de dados
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
     yield # API fica online aqui
+
+    # 3. Fecha as conexões Redis de forma segura
+    await app.state.redis.aclose()
 
