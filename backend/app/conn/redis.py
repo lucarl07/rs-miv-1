5a2f66c7b25108b5739759b96502661a40d582b6
lucarl07 <07lucarl.dev@gmail.com>
2026-07-03 16:28:35 -0300

    fix(conn): .env vars not loaded in redis.py

--- backend/app/conn/redis.py
+++ backend/app/conn/redis.py
@@ -2,8 +2,10 @@
 import os
 
 # Bibliotecas do Projeto
+from dotenv import load_dotenv
 import redis.asyncio as redis
 
+load_dotenv()
 REDIS_URL = os.environ.get("REDIS_URL", "redis://localhost:6379/0")
 
 r = redis.from_url(REDIS_URL, decode_responses=True)
