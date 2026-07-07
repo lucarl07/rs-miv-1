bfd5cef40050fa70f47e6ba53d73f40a0fe40d79
lucarl07 <07lucarl.dev@gmail.com>
2026-07-07 18:31:13 -0300

    feat(backend): New repo query get_last_n_messages

--- backend/app/repositories/message.py
+++ backend/app/repositories/message.py
@@ -1,5 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 from app.models.message import Message
+from app.schemas.message import ChatMessagePayload, MessageData
 
 async def save_message(db: AsyncSession, nickname: str, content: str) -> Message:
     message = Message(nickname=nickname, content=content) # Timestamp será gerada automaticamente no modelo Message
@@ -7,3 +9,23 @@ async def save_message(db: AsyncSession, nickname: str, content: str) -> Message
     await db.commit()
     return message;
 
+async def get_last_n_messages(db: AsyncSession, n: int = 100) -> list[dict]:
+    result = await db.execute(
+        select(Message)
+        .order_by(Message.timestamp.desc())
+        .limit(n)
+    )
+    messages = result.scalars().all()
+
+    return [
+        ChatMessagePayload(
+            type="message",
+            data=MessageData(
+                id=msg.id,
+                nickname=msg.nickname,
+                content=msg.content,
+                timestamp=msg.timestamp,
+            ),
+        ).model_dump(mode="json")
+        for msg in reversed(messages)
+    ]
