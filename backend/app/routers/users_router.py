a8e91df91cf0193d28de95e350906bfe3c5f4885
lucarl07 <07lucarl.dev@gmail.com>
2026-07-08 17:53:10 -0300

    refactor(backend): Consume get_current_user dep at GET /users/me
    ...which is now attributed to the function read_current_user

--- backend/app/routers/users_router.py
+++ backend/app/routers/users_router.py
@@ -2,41 +2,17 @@
 from typing import Annotated
 
 # Bibliotecas Externas
-from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi import security
-from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
-from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import APIRouter, Depends
 
 # Módulos do Projeto
-from app.conn.db import get_db
+from app.dependencies import get_current_user
 from app.models.user import User
-from app.repositories.user import get_user_by_id
 from app.schemas.user import UserOut
-from app.utils.jwt import decode_access_token
 
 router = APIRouter(prefix="/users", tags=["users"])
-security = HTTPBearer()
 
 @router.get("/me", response_model=UserOut)
-async def get_current_user(
-    auth: Annotated[HTTPAuthorizationCredentials, Depends(security)],
-    db: AsyncSession = Depends(get_db)
+async def read_current_user(
+    current_user: Annotated[User, Depends(get_current_user)]
 ) -> User:
-    access_token = auth.credentials
-    token_data = decode_access_token(access_token)
-
-    if token_data is None:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Credencial de acesso inválida e/ou expirada."
-        )
-
-    current_user = await get_user_by_id(db, token_data.user_id)
-
-    if current_user is None:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Credencial de acesso inválida e/ou expirada."
-        )
-
     return current_user
