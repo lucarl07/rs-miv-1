ab87db29b8f73ea039c3ff9f3dd2726b23db4e1e
lucarl07 <07lucarl.dev@gmail.com>
2026-07-09 12:08:32 -0300

    docs(backend): Regarding the sanitizer not accepting any HTML tags

--- backend/app/utils/sanitization.py
+++ backend/app/utils/sanitization.py
@@ -1,4 +1,8 @@
 import nh3
 
 msg_cleaner = nh3.Cleaner(tags=set())
+# Por enquanto, nenhuma tag HTML será permitida.
+#
+# Possivelmente isso mude no futuro, caso eu decida liberar formatação
+# HTML nas mensagens, permitir envio de snippets de código, etc.
 
