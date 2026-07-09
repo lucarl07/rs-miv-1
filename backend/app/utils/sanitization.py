20ee3ed014b10efe500dbfec85001e9120997610
lucarl07 <07lucarl.dev@gmail.com>
2026-07-09 14:55:10 -0300

    docs(backend): Document app/utils/sanitization.py

--- backend/app/utils/sanitization.py
+++ backend/app/utils/sanitization.py
@@ -1,8 +1,10 @@
 import nh3
 
-msg_sanitizer = nh3.Cleaner(tags=set())
-# Por enquanto, nenhuma tag HTML será permitida.
-#
-# Possivelmente isso mude no futuro, caso eu decida liberar formatação
-# HTML nas mensagens, permitir envio de snippets de código, etc.
+msg_sanitizer = nh3.Cleaner(tags=set()) 
+"""Sanitizador de todos os tipos de mensagem."""
+# ---
+# Por enquanto, nenhuma tag HTML será permitida. Possivelmente isso mude no
+# futuro, caso eu decida liberar formatação HTML nas mensagens, permitir 
+# envio de snippets de código, etc.
+# ---
 
