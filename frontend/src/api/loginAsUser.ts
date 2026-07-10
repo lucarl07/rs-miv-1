c0a9bb9fc6c342d5bc280032a592d6c382d4163c
lucarl07 <07lucarl.dev@gmail.com>
2026-07-10 17:54:25 -0300

    feat(backend/api): Pass all errors through formatPydanticErrors

--- frontend/src/api/loginAsUser.ts
+++ frontend/src/api/loginAsUser.ts
@@ -15,9 +15,8 @@ async function loginAsUser(
   if (!res.ok) {
     const err = await res.json()
     throw new Error(
-      err.detail ??                       // Caso err.detail seja uma string simples
-      formatPydanticErrors(err.detail) ?? // Caso err.detail seja um retorno (objeto) do Pydantic
-      'Credenciais inválidas.'            // Caso err.detail seja outro formato
+      formatPydanticErrors(err.detail) // Caso err.detail seja um retorno (objeto) do Pydantic
+      ?? 'Credenciais inválidas.'      // Caso err.detail seja outro formato
     )
   }
 
