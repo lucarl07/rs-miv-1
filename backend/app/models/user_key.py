841f08eb62d8f70650ffee7117d9fe711b226a2f
lucarl07 <07lucarl.dev@gmail.com>
2026-07-13 16:39:17 -0300

    fix(db): Column types & constraints at user_keys

--- backend/app/models/user_key.py
+++ backend/app/models/user_key.py
@@ -7,9 +7,20 @@ from app.conn.db import Base
 class UserKey(Base):
     __tablename__ = 'user_keys'
 
-    id: Mapped[str] = mapped_column(String(36), primary_key=True, default=lambda: str(uuid4()))
-    user_id: Mapped[str] = mapped_column(ForeignKey("users.id"), unique=True, nullable=False)
-    public_key: Mapped[str] = mapped_column(Text, nullable=True)
+    id: Mapped[str] = mapped_column(
+        String(36), 
+        primary_key=True, 
+        default=lambda: str(uuid4())
+    )
+    user_id: Mapped[str] = mapped_column(
+        String(36), 
+        ForeignKey("users.id"), 
+        unique=True, nullable=False
+    )
+    public_key: Mapped[str] = mapped_column(
+        Text, 
+        nullable=False
+    )
 
     # Por enquanto o relacionamento com User vai ser 1:1, mas em um futuro
     # próximo trocarei para 1:N (um User, uma ou mais UserKeys)
