1d48d2536963e51af642a51b014cb781301866a2
lucarl07 <07lucarl.dev@gmail.com>
2026-07-13 16:28:15 -0300

    fix(db): Generate UUID for 'id' field of 'user_keys'

--- backend/app/models/user_key.py
+++ backend/app/models/user_key.py
@@ -1,12 +1,13 @@
+from uuid import uuid4
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import ForeignKey, Text
+from sqlalchemy import ForeignKey, String, Text
 from app.conn.db import Base
 
 
 class UserKey(Base):
     __tablename__ = 'user_keys'
 
-    id: Mapped[str] = mapped_column(primary_key=True)
+    id: Mapped[str] = mapped_column(String(36), primary_key=True, default=lambda: str(uuid4()))
     user_id: Mapped[str] = mapped_column(ForeignKey("users.id"), unique=True, nullable=False)
     public_key: Mapped[str] = mapped_column(Text, nullable=True)
 
