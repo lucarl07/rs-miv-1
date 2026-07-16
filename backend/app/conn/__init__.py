7548f4c0debe6a70732a453849721f090e7c4e89
lucarl07 <07lucarl.dev@gmail.com>
2026-07-16 11:56:43 -0300

    fix(pgp): Session key is only generated on its absence on Redis
    Previously, the session key was always regenerated when the server
    restarted. This caused the messages to be unreadable to the front-end,
    due to the fact that they would be encrypted using an invalid session
    key.

--- backend/app/conn/__init__.py
+++ backend/app/conn/__init__.py
@@ -1,6 +1,7 @@
 # Bibliotecas Nativas
 import base64
 import secrets
+import logging
 from contextlib import asynccontextmanager
 
 # Bibliotecas do Projeto
@@ -11,6 +12,8 @@ from .redis import r
 from .db import Base, engine
 import app.models # Importação necessária para sincronização não-lazy
 
+logger = logging.getLogger(__name__)
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     """Função de ciclo de vida da aplicação."""
@@ -19,10 +22,18 @@ async def lifespan(app: FastAPI):
     app.state.redis = r
     await app.state.redis.ping()
 
-    # Chave AES-256 para PGP gerada e adicionada ao Redis:
-    session_key = secrets.token_bytes(32) # 256 bits
-    b64_session_key = base64.b64encode(session_key).decode('ascii')
-    await app.state.redis.set("session_key:global", b64_session_key)
+    # Buscando por chave AES-256 para PGP:
+    existing_session_key = await r.get("session_key:global")
+
+    if existing_session_key is None:
+        # ...se não existir, é gerada e adicionada ao Redis:
+        session_key = secrets.token_bytes(32) # 256 bits
+        b64_session_key = base64.b64encode(session_key).decode('ascii')
+
+        await app.state.redis.set("session_key:global", b64_session_key)
+        logger.info('No existing K found in Redis - new K generated at boot')
+    else:
+        logger.info('Existing K reused from Redis - history remains decipherable')
     
     # Conexão com o banco de dados efetuada:
     async with engine.begin() as conn: 
