b6409f2f633f17ccedeafddd50113b1d68377124
lucarl07 <07lucarl.dev@gmail.com>
2026-07-17 14:54:23 -0300

    fix(frontend): Check if K is available before marking the chat as ready

--- frontend/src/views/ChatView.vue
+++ frontend/src/views/ChatView.vue
@@ -13,18 +13,24 @@
   import useAuth from '@/composables/useAuth.ts'
   import useWebSocket from '@/composables/useWebSocket'
   import useAutoScroll from '@/composables/useAutoScroll.ts'
+  import { useSessionKey } from '@/composables/useSessionKey'
   import genUniqueNickname from '@/utils/genUniqueNickname.ts'
 
-  const { status, messages, onlineUsers, send, disconnect } = useWebSocket()
+  const { sessionKey } = useSessionKey()
+  const { logout } = useAuth()
   const { nickname } = useUserData({ loadOnStartup: true })
 
-  const { logout } = useAuth()
+  const { status, messages, onlineUsers, send, disconnect } = useWebSocket()
   const { scrollTarget } = useAutoScroll(messages.value, () => true)
 
-  const isChatReady = computed<boolean>(() => status.value === 'Conectado')
+  const isChatReady = computed<boolean>(() =>
+    status.value === 'Conectado' && sessionKey.value !== null
+  )
 
-  const handleSend = async (content) => await send(content)
-  const handleLogout = () => {
+  async function handleSend(content) {
+    await send(content)
+  }
+  function handleLogout() {
     const isConfirmed = confirm("Tem certeza de que você deseja sair?")
     if (!isConfirmed) return;
 
