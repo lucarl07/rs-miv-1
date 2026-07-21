b571b3758add480eff6f053d931d15973d23bfbb
lucarl07 <07lucarl.dev@gmail.com>
2026-07-21 18:28:27 -0300

    feat(frontend/FormInput): Add prop isRequired + computed value labelTitle

--- frontend/src/components/FormInput.vue
+++ frontend/src/components/FormInput.vue
@@ -1,14 +1,25 @@
 <script setup lang="ts">
+  import { computed } from 'vue'
+
   defineOptions({
     inheritAttrs: false
   })
 
   const model = defineModel()
-  const props = defineProps<{
+  const {
+    fieldname, labelText, showOnError, isRequired = true
+  } = defineProps<{
     fieldname: string
     labelText?: string
     showOnError?: string
+    isRequired?: boolean
   }>()
+
+  const labelTitle = computed<string>(() =>
+    isRequired
+    ? 'Este campo é obrigatório.'
+    : 'Este campo é opcional.'
+  )
 </script>
 
 <template>
@@ -16,15 +27,19 @@
     <label
       v-if="labelText"
       :for="fieldname"
+      :title="labelTitle"
       class="block text-sm font-medium text-grey-100"
     >
       {{ labelText }}
+      <span v-if="isRequired" class="text-red-400">*</span>
     </label>
     <!-- Atributos de passagem serão herdados no input abaixo: -->
     <input
       v-model="model"
       v-bind="$attrs"
       :id="fieldname"
+      :required="isRequired"
+      :autocomplete="$attrs.autocomplete || fieldname"
       class="
         mt-1.5 px-3 py-2 w-full rounded-md border border-mauve-500
         bg-pale-silver
