09c1b781843eb46b772e3ae2df38d71f33cdb6b0
lucarl07 <07lucarl.dev@gmail.com>
2026-07-21 16:42:15 -0300

    feat(frontend): Add new component FormInput

---
+++ frontend/src/components/FormInput.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+  defineOptions({
+    inheritAttrs: false
+  })
+
+  const model = defineModel()
+  const props = defineProps<{
+    fieldname: string
+    labelText?: string
+    showOnError?: string
+  }>()
+</script>
+
+<template>
+  <div :id="'wrapper_' + fieldname">
+    <label
+      v-if="labelText"
+      :for="fieldname"
+      class="block text-sm font-medium text-grey-100"
+    >
+      {{ labelText }}
+    </label>
+    <!-- Atributos de passagem serão herdados no input abaixo: -->
+    <input
+      v-model="model"
+      v-bind="$attrs"
+      :id="fieldname"
+      class="
+        mt-1.5 px-3 py-2 w-full rounded-md border border-mauve-500
+        bg-pale-silver
+        focus:outline-double focus:outline-crushed-berry
+      "
+    />
+    <slot name="customBottomText">
+      <p
+        v-if="showOnError"
+        class="mt-1.5 text-sm"
+      >
+        {{ showOnError }}
+      </p>
+    </slot>
+  </div>
+</template>
+
