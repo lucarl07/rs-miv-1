9922cc8b1864d8434feae9d51eced5bb0a7e76e3
lucarl07 <07lucarl.dev@gmail.com>
2026-07-23 11:32:15 -0300

    feat(db): Set 1:N relationship from User to its Messages

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String, DateTime
 from datetime import datetime, timezone
+from typing import List
 from uuid import uuid4
 from app.conn.db import Base
 
@@ -8,6 +9,7 @@ from app.conn.db import Base
 class User(Base):
     __tablename__ = "users"
 
+    # ==== Colunas ====
     id: Mapped[str] = mapped_column(String(36), primary_key=True, default=lambda: str(uuid4()))
     nickname: Mapped[str] = mapped_column(String(30), unique=True, index=True, nullable=False)
     email: Mapped[str] = mapped_column(String(255), unique=True, index=True, nullable=False)
@@ -16,7 +18,11 @@ class User(Base):
         DateTime(timezone=True), default=lambda: datetime.now(timezone.utc)
     )
 
+    # ==== Relacionamentos ====
     key: Mapped["UserKey"] = relationship(
-        back_populates="user", uselist=False, cascade="all, delete-orphan"
+        back_populates="user", 
+        uselist=False, cascade="all, delete-orphan"
     )
-
+    messages: Mapped[List["Message"]] = relationship(
+        back_populates="user"
+    )
