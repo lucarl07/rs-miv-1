f520a2e281eb2b1d25fa0da1480dedcc712e350d
lucarl07 <07lucarl.dev@gmail.com>
2026-07-24 17:25:23 -0300

    fix(backend): start.sh script

--- backend/start.sh
+++ backend/start.sh
@@ -1,5 +1,9 @@
+# Set default values
+HOST=0.0.0.0
+PORT=8000
+
 # Migrate database
 alembic upgrade head
 
 # Run API with Uvicorn
-uvicorn app.main:app --host 0.0.0.0 --port $PORT
+uvicorn app.main:app --host $HOST --port $PORT
