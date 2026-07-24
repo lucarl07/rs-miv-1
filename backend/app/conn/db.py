3e591bcdbc9fc33494ef73fef158bf8214773a22
lucarl07 <07lucarl.dev@gmail.com>
2026-07-24 19:24:03 -0300

    feat(backend): Handle SSL treatment for production DBs

--- backend/app/conn/db.py
+++ backend/app/conn/db.py
@@ -1,5 +1,6 @@
 # Bibliotecas Nativas
 import os
+import re
 from typing import AsyncGenerator 
 
 # Bibliotecas do Projeto
@@ -15,10 +16,17 @@ load_dotenv()
 DB_URL = os.environ["DB_URL"]
 DB_ECHO = os.environ.get("DB_ECHO", '0').strip().lower() in ('1', 'true', 'yes')
 
+# Valida quais opções serão necessárias para efetuar conexão
+connect_args = {}
+
+if "sslmode=require" in DB_URL:
+    DB_URL = re.sub(r"[?&]sslmode=require", "", DB_URL)
+    connect_args = {"ssl": "require"}
+
 # Objetos para a conexão com o banco de dados
 engine = create_async_engine(
     DB_URL, 
-    connect_args={}, 
+    connect_args=connect_args, 
     echo=DB_ECHO
 )
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
