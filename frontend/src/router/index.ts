acfbcf9cbe9827e2fbf30266bcf5a4f6a2ca7146
lucarl07 <07lucarl.dev@gmail.com>
2026-07-27 16:39:52 -0300

    feat(frontend)(TBD): Route page titles

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,22 +2,33 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import useAuth from '@/composables/useAuth'
 
+const PAGE_TITLE = import.meta.env.PAGE_TITLE
+
 const routes = [
   { path: '/', redirect: '/chat' },
   {
     path: '/signup',
     name: 'signup',
-    component: () => import('@/views/SignupView.vue')
+    component: () => import('@/views/SignupView.vue'),
+    meta: {
+      title: `Cadastrar - ${PAGE_TITLE}`
+    }
   },
   {
     path: '/login',
     name: 'login',
-    component: () => import('@/views/LoginView.vue')
+    component: () => import('@/views/LoginView.vue'),
+    meta: {
+      title: `Entrar - ${PAGE_TITLE}`
+    }
   },
   {
     path: '/chat',
     name: 'chat',
-    component: () => import('@/views/ChatView.vue')
+    component: () => import('@/views/ChatView.vue'),
+    meta: {
+      title: `Chat Global - ${PAGE_TITLE}`
+    }
   },
 ]
 
